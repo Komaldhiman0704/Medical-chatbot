be7622cb15669b92b037ffe2707c195df14cf09a
KOMAL <dhimankomal443@gmail.com>
2026-01-18 12:52:25 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Gen AI Project',
+    version= '0.0.0',
+    author= 'komal',
+    author_email= 'dhimankomal443@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
